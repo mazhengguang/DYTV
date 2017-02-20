5c501ebf9fc2bc80732b5f14986758ae1654543a
mzg <912235156@qq.com>
2017-02-20 17:51:18 +0800

    创建并初始化项目

---
+++ DDTV/DDTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DDTV
+//
+//  Created by xthink3 on 2017/2/20.
+//  Copyright © 2017年 xxkj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
